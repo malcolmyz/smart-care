4546291371850ccd982aa0dc0342bfd9b474a6c3
Malcolm <52551489+malcolmyz@users.noreply.github.com>
2025-12-08 19:51:36 +0900

    Feature/initial screen prototyping (#2)
    * feat:初期ログイン画面実装
    
    * feat:ダッシュボード初期画面作成
    
    * feat:初期ディレクトリを追加
    
    * feat:CSSをモジュールに分割
    
    * fix:古いdashboardディレクトリを削除
    
    * CSSファイルの格納場所変更、dashboard配下のディレクトリをdashboardと同列に変更

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+// app/login/page.tsx
+import { useState } from "react";
+import styles from "@/app/styles/login.module.css";
+
+export default function LoginPage() {
+  const [staffId, setStaffId] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className={styles["login-page"]}>
+      <div className={styles["login-container"]}>
+        <div className={styles.logo}>
+          <h1>SmartCARE</h1>
+          <p>介護記録システム</p>
+        </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault(); // 画面がリロードされるのを防ぐ
+            console.log({ staffId, password });
+          }}
+        >
+          <div className={styles["form-group"]}>
+            <input
+              type="text"
+              placeholder="スタッフID"
+              value={staffId}
+              onChange={(e) => setStaffId(e.target.value)}
+              required
+            />
+          </div>
+          <div className={styles["form-group"]}>
+            <input
+              type="password"
+              placeholder="パスワード"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className={styles["login-button"]}>ログイン</button>
+        </form>
+        <div className={styles["forgot-password"]}>
+            <a href="#">パスワードをお忘れですか？</a>
+        </div>
+      </div>
+    </div>
+  );
+}
